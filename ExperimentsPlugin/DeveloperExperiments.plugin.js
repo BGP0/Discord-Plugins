474d2948cac0f4af518dfc7cd88ea5ee35f82698
BGP0 <59474088+BGP0@users.noreply.github.com>
2022-10-22 15:48:27 +0100

    added mass delete

--- ExperimentsPlugin/DeveloperExperiments.plugin.js
+++ ExperimentsPlugin/DeveloperExperiments.plugin.js
@@ -1,13 +1,17 @@
 /**
  * @name DeveloperExperiments
  * @description Grants access to features for discord staff such as experiments tab
- * @author BGP2, CAEC64
- * @version 1.0.0
- * @source https://github.com/BGP0/Discord-Plugins/rawfile
- * @updateUrl https://github.com/BGP0/Discord-Plugins/rawfile
+ * @author BGP, CAEC64
+ * @version 1.0.4
+ * @source https://github.com/BGP0/Discord-Plugins/blob/main/ExperimentsPlugin/DeveloperExperiments.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/BGP0/Discord-Plugins/main/ExperimentsPlugin/DeveloperExperiments.plugin.js
  */
+const version = Number("1.0.4".replaceAll('.', ''))
+const fs = require("fs")
 
 function setDev(b) {
+	// Current method mostly by me, sets the staff flag and then reloads the developer experiments.
+	// Requries no libraries, but idk how to undo it without reloading discord
 	window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { // Weird thing to get loads of webpack data
 		var stuff = Object.values(req.c) // We don't care about the keys of the webpack data
 
@@ -16,10 +20,20 @@ function setDev(b) {
 
 		user.flags += b == true ? 1 : -1 // Add/Remove the staff key (2^0)
 		mall.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]() // Open the store with our new key, idk how to close yet
+		try {mall.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({user: {flags: 1}})} catch(_) {} // will throw some errors as the function expects more parameters, but it will still let us use buttons even with the exception
 	}])
 }
 
 module.exports = class {
+	load() { // Because @updateUrl still isn't implemented and using a zeres library is bloat + requires plugin to be verified
+		fetch("https://bgp0.github.io/Discord-Plugins/ExperimentsPlugin/DeveloperExperiments.plugin.js", {cache: "no-store"}).then(res => res.text()).then(res => {
+			let newVersion = Number(res.substring(res.indexOf("version") + 8, res.indexOf("version") + 13).replaceAll('.', ''))
+			if (newVersion > version) {
+				console.log("UPDATING!")
+				fs.writeFile(`${BdApi.Plugins.folder}/DeveloperExperiments.plugin.js`, res)
+			}
+		})
+	}
 	start() {
 		setDev(1)
 	}
