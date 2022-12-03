c330701435b7fb10301425db3476fd9fcc16176a
BGP0 <59474088+BGP0@users.noreply.github.com>
2022-12-03 21:29:11 +0000

    template plugin

---
+++ Template/Template.plugin.js
@@ -0,0 +1,39 @@
+/**
+ * @name Template
+ * @author BGP
+ * @description Just a template
+ * @version 1.0.0
+ * @source https://github.com/BGP0/Discord-Plugins/blob/main/Template/Template.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/BGP0/Discord-Plugins/main/Template/Template.plugin.js
+ */
+
+module.exports = class TemplatePlugin {
+    async start() {
+        test = await ZeresPluginLibrary.PluginUpdater.checkForUpdate(this.config.name, this.config.version, this.config.updateUrl)
+        console.log(test)
+        console.log("Hello world!")
+    }
+    stop() {}
+    
+    load() {
+        if (!global.ZeresPluginLibrary) {
+            BdApi.showConfirmationModal("Library plugin is needed",
+                `ZeresPluginLibrary is missing. Please click Download Now to install it.`, {
+                confirmText: "Download",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    request.get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", (error, response, body) => {
+                        if (error) {
+                            return electron.shell.openExternal("https://github.com/rauenzi/BDPluginLibrary");
+                        }
+                        fs.writeFileSync(path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body);
+                    });
+                }
+            });
+        }
+    }
+
+    constructor(c) {
+        this.config = c
+    }
+}
